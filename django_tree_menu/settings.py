380cc5b278678f726e085e4c03385c09ce700816
SammyJankins <neroragno@mail.ru>
2023-03-15 02:12:38 +0700

    Cat and subcat titles

--- django_tree_menu/settings.py
+++ django_tree_menu/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uj^vib==qk%sshd3jzv!&$a#jt!o)cmwdk+-rp8dh_7fh6^2-f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
